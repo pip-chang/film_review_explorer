8cd3efa10c655042c41ee53f301ce75db1a284c9
prog1chang <prog1chang@cl.uni-heidelberg.de>
2023-07-19 00:46:05 +0200

    Update

---
+++ docs/source/modules.rst
@@ -0,0 +1,7 @@
+film_review_explorer
+===================
+
+.. toctree::
+   :maxdepth: 4
+
+   film_review_explorer
--- src/film_review_explorer/topic_modeler.py
+++ src/film_review_explorer/topic_modeler.py
@@ -3,19 +3,34 @@ import pandas as pd
 from typing import Iterable, Generator, List
 from spacy.tokens import Doc, Token
 
-# load processed dataframes in and prepare for topic modeling
-
-# functions for every step of BERTopic
-
-
 class TextProcessor:
+    """A class for text processing using spaCy."""
+
     def __init__(self, model: str):
+        """
+        Initializes a TextProcessor instance.
+
+        Args:
+            model (str): The name of the spaCy model to load.
+        """
         self.nlp = spacy.load(model)
 
     @staticmethod
     def check_token(
         token: Token, punct: bool = True, num: bool = True, stopword: bool = True
     ) -> bool:
+        """
+        Checks if a token meets specified criteria.
+
+        Args:
+            token (Token): The token to check.
+            punct (bool): Whether to consider punctuation tokens (default=True).
+            num (bool): Whether to consider numerical tokens (default=True).
+            stopword (bool): Whether to consider stopword tokens (default=True).
+
+        Returns:
+            bool: True if the token meets the criteria, False otherwise.
+        """
         if punct:
             punct = token.is_punct
         if num:
@@ -26,10 +41,28 @@ class TextProcessor:
 
     @staticmethod
     def get_entities(doc: Doc) -> List[str]:
+        """
+        Retrieves the entities from a spaCy document.
+
+        Args:
+            doc (Doc): The spaCy document.
+
+        Returns:
+            List[str]: The list of entity texts.
+        """
         return [ent.text for ent in doc.ents]
 
     @staticmethod
     def get_sentences(doc: Doc) -> List[str]:
+        """
+        Retrieves the sentences from a spaCy document.
+
+        Args:
+            doc (Doc): The spaCy document.
+
+        Returns:
+            List[str]: The list of sentence texts.
+        """
         return [sentence.text for sentence in doc.sents]
 
     def get_tokens(
@@ -40,6 +73,19 @@ class TextProcessor:
         num: bool = True,
         stopword: bool = True,
     ) -> List[str]:
+        """
+        Retrieves the tokens from a spaCy document.
+
+        Args:
+            doc (Doc): The spaCy document.
+            lemma (bool): Whether to use lemmas instead of raw texts (default=True).
+            punct (bool): Whether to exclude punctuation tokens (default=True).
+            num (bool): Whether to exclude numerical tokens (default=True).
+            stopword (bool): Whether to exclude stopword tokens (default=True).
+
+        Returns:
+            List[str]: The list of token texts.
+        """
         if lemma:
             return [
                 token.lemma_
@@ -62,6 +108,20 @@ class TextProcessor:
         num: bool = True,
         stopword: bool = True,
     ) -> List[str]:
+        """
+        Retrieves n-grams from a spaCy document.
+
+        Args:
+            doc (Doc): The spaCy document.
+            n (int): The size of n-grams to retrieve.
+            sep (str): The separator string between tokens (default=" ").
+            punct (bool): Whether to exclude punctuation tokens (default=True).
+            num (bool): Whether to exclude numerical tokens (default=True).
+            stopword (bool): Whether to exclude stopword tokens (default=True).
+
+        Returns:
+            List[str]: The list of n-grams.
+        """
         ngrams = []
         for sentence in doc.sents:
             for i in range(len(sentence) - n + 1):
@@ -76,6 +136,17 @@ class TextProcessor:
     def process_zh(
         self, texts: Iterable[str], disable: Iterable[str] = []
     ) -> Generator:
+        """
+        Processes a batch of Chinese texts.
+
+        Args:
+            texts (Iterable[str]): The texts to process.
+            disable (Iterable[str]): Components to disable during processing (default=[]).
+
+        Yields:
+            Generator: A generator yielding the processed results in the following order:
+                      tokens, entities, bigrams, trigrams, quadgrams.
+        """
         for doc in self.nlp.pipe(texts, disable=disable):
             tokens = self.get_tokens(doc, lemma=False)
             entities = self.get_entities(doc)
@@ -88,6 +159,17 @@ class TextProcessor:
     def process_en(
         self, texts: Iterable[str], disable: Iterable[str] = []
     ) -> Generator:
+        """
+        Processes a batch of English texts.
+
+        Args:
+            texts (Iterable[str]): The texts to process.
+            disable (Iterable[str]): Components to disable during processing (default=[]).
+
+        Yields:
+            Generator: A generator yielding the processed results in the following order:
+                      tokens, entities, bigrams, trigrams, quadgrams.
+        """
         for doc in self.nlp.pipe(texts, disable=disable):
             tokens = self.get_tokens(doc)
             entities = self.get_entities(doc)
